f32ad7b6b25fae09f479271ee55b14c545e5c87b
Anuj Sevak <arsevak@mun.ca>
2023-03-18 14:58:57 -0230

    Websocket send pdf - Proof of Concept

--- ResumeScorrer.js
+++ ResumeScorrer.js
@@ -8,40 +8,29 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', (socket) => {
   console.log('Client connected');
 
-  socket.on('send_message', (message) => {
-    const data = JSON.parse(message);
+  socket.on('message', async function (message) {
+    //const data = JSON.parse(message);
+    const file = message;
 
     // Check if a file was sent
-    if (data.file) {
-      console.log('Received file from client:', data.file);
+    if (file) {
+      console.log('Received file from client!');
 
       // Convert the file buffer back to PDF
-      const pdf = new Uint8Array(data.file);
-      pdfjsLib.getDocument(pdf).promise.then((pdfDoc) => {
-        const numPages = pdfDoc.numPages;
-        console.log(`PDF document loaded with ${numPages} pages`);
+      const data = new Uint8Array(file);
+      const pdf = await pdfjsLib.getDocument(data).promise;
+      // Extract the text from each page of the PDF
+      const numPages = pdf.numPages;
+      let extractedText = '';
+      for (let i = 1; i <= numPages; i++) {
+        const page = await pdf.getPage(i);
+        const content = await page.getTextContent();
+        const pageText = content.items.map(item => item.str).join('\n');
+        extractedText += pageText;
+      }
 
-        // Extract text from each page of the PDF
-        let extractedText = '';
-        for (let i = 1; i <= numPages; i++) {
-          pdfDoc.getPage(i).then((page) => {
-            return page.getTextContent();
-          }).then((textContent) => {
-            extractedText += textContent.items.map((item) => {
-              return item.str;
-            }).join(' ');
-          });
-        }
-
-        // Send the extracted text back to the client
-        if (data.sendBack) {
-          socket.send(JSON.stringify({
-            message: `Extracted text: ${extractedText}`
-          }));
-        }
-      }).catch((error) => {
-        console.error('Error while loading PDF:', error);
-      });
+      // Send the extracted text back to the client
+      socket.send(JSON.stringify(extractedText));
     }
   });
 
